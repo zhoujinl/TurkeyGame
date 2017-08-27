fe7eafa3b963a4f61394d569356a69737e8f8ea2
zhoujinl <zhoujinl@126.com>
2017-08-27 16:40:12 +0800

    update

---
+++ turkeyGame/datafile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#coding=utf-8
+'''
+供 tukeyGame 调用
+使用 openpyxl 包保存中间数据到 xlsx ，以便用excel 分析统计
+openpyxl参考手册：https://openpyxl.readthedocs.io/en/default/tutorial.html#create-a-workbook
+
+'''
+
+from openpyxl import Workbook
+
+class DataFile:
+    '状态数据保存'
+    
+    def __init__(self,filename):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.title = "Tukey"
+        self.ws.append(["当前期数", "当前总盈利","本期赚钱","本期成本","本期单注金额","本期中奖概率" ])    
+        self.filename = filename
+       
+    def write2xlsx(self, **other):
+        '每次中奖的概率'
+        # Rows can also be appended
+        self.ws.append([other["numberPeriod"],other["money"],other["lastIswinMoney"],other["cost"],other["betting"],other["probability"]])    
+        
+    def save2xlsx(self):
+        self.wb.save(self.filename)
+    
+    def __del__(self):
+        self.wb.close()
+        
+datafile = DataFile(r"E:\Git\turkeyGame\sample.xlsx")
+datafile.save2xlsx()
--- turkeyGame/turkeyGame.py
+++ turkeyGame/turkeyGame.py
@@ -15,7 +15,8 @@ params: python calc.py -h
 '''
 import openpyxl 
 import random
-
+import datetime
+from datafile import DataFile
 totalNumber = 49.0
 randomNumber = 100
 
@@ -27,34 +28,42 @@ class Turkey:
         self.money = 0
         self.lastIswinMoney = 0
         self.winCount = 0
+        self.playCount = 0
         self.options = options
+        stime = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')
+        self.datafile = DataFile(r"E:\Git\turkeyGame\var\sample-" +stime +"-"+ str(name) + ".xlsx") 
         self.originBetting = options.betting
         self.probabilityOfWinningEachPeriod()
-        self.probability = self.options.numberBetting/totalNumber
+        #self.probability = self.options.numberBetting/totalNumber
         #self.probability = 0.5
 
     def probabilityOfWinningEachPeriod(self):
         '每次中奖的概率'
-        self.probability = self.options.numberBetting/totalNumber 
-        print '每次中奖的概率: ' + str(self.probability)
+        #self.probability = self.options.numberBetting/totalNumber 
+        self.probability = 0.3
+        print '每次中奖的概率' + str(self.probability)
 
     def isWin(self):
         '本期是否中奖'
         #return random.randint(0, 100) < (self.probability * 100)
-        print (random.randint(0, 100) < (self.probability * 100))
-        return "False"
+        return random.randint(0, 100) < (self.probability * 100)
         
     def makeMoney(self):  
         '本期是否赚钱。中则加钱，不中则扣钱'
-        print ("当期押注大小：" + str(tukey.options.betting))
-        if self.isWin():
+        
+        if self.isWin(): 
             makeMoney = self.options.betting * self.options.odds - self.options.betting * self.options.numberBetting
             self.winCount = self.winCount + 1 
         else :
             makeMoney = - (self.options.betting * self.options.numberBetting)
         self.lastIswinMoney = makeMoney
+        self.playCount = self.playCount + 1
         self.money = self.money + makeMoney
-
+        
+        #记录每局输赢
+        self.datafile.write2xlsx(numberPeriod=self.playCount,money=self.money,lastIswinMoney=self.lastIswinMoney,
+                                    betting=self.options.betting,probability=self.probability,cost=self.options.betting * self.options.numberBetting)
+        
     def makeMoneyByStrategy(self):
         '启用策略，当某几期之后，亏钱时，增大赌注，默认是翻倍;'   
         self.makeMoney()
@@ -69,21 +78,55 @@ class Turkey:
         '''
         
         # 赌注每次增加一倍
-        if self.money < 0 :
+        if self.money < 0 :  #亏钱的情况下加大筹码
             if self.options.betting < self.options.maxBetting  :
                 self.options.betting = self.options.betting +  self.originBetting             
-        elif self.options.betting > self.originBetting  :
-            self.options.betting = self.options.betting - self.originBetting    
-            
+        else  :              #赚钱的情况下怎么处理    
+            if self.options.betting <= self.options.maxBetting  :
+                if self.lastIswinMoney > 0 : #如果上一期中了,加大筹码
+                    self.options.betting = self.options.betting + self.originBetting  
+                else : # 反之
+                    self.options.betting = self.options.betting + self.originBetting  
+                    
     def startGame(self):
         if self.options.autoIncreaseBetting == "true" :
+            ''' ##有限游戏次数下的游戏
             for i in range(0,self.options.gameCount) :
                 self.makeMoneyByStrategy()
+            '''
+            
+            '''##赌徒模式 假如要赚到10000 才收手
+            self.makeMoneyByStrategy()
+            while self.money < 10000  :
+                self.makeMoneyByStrategy()
+                if self.money < -10000:
+                    break
+            '''
+            '''## 有赚就跑模式 
+            ## 看来一定要止损10000 ，止盈可以多一点
+            self.makeMoneyByStrategy()
+            while self.money < 100000  :
+                self.makeMoneyByStrategy()
+                if self.money < -10000:
+                    break                   
+            '''
+            
+            ##然而时间有限
+            self.makeMoneyByStrategy()
+            while self.money < 10000  :
+                self.makeMoneyByStrategy()
+                if self.money < -3000 :
+                    break  
+                if self.playCount > 30 :
+                    break
         else :        
             for i in range(0,self.options.gameCount) :
                 self.makeMoney() 
         return self.money
      
+    def __del__(self):
+        self.datafile.save2xlsx()
+        
 if __name__ == '__main__':
     from optparse import OptionParser
     import os
@@ -91,17 +134,42 @@ if __name__ == '__main__':
     parser = OptionParser()
     usage = "usage: %prog [options] arg1" 
     parser.add_option("-o", "--odds", dest="odds", help="The odds ;default 48", default='48', type="int")
-    parser.add_option("-n", "--numberBetting", dest="numberBetting", help="The number of betting;default 12", default='12', type="int")
-    parser.add_option("-b", "--betting", dest="betting", help="Each bet;init 10", default='10', type="int")
+    parser.add_option("-n", "--numberBetting", dest="numberBetting", help="The number of betting;default 12", default='13', type="int")
+    parser.add_option("-b", "--betting", dest="betting", help="Each bet;init 10", default='20', type="int")
     parser.add_option("-a", "--auto", dest="autoIncreaseBetting", help="IS atuo Increase Each bet;default true", default='true', type="string")
     parser.add_option("-m", "--maxBetting", dest="maxBetting", help="The max Betting;default 100", default='100', type="int")
     parser.add_option("-c", "--count", dest="gameCount", help="The gameCount;default 100", default='10', type="int")
 
     (options, args) = parser.parse_args()
+    #只开一局
+    '''
     tukey=Turkey("xiaolizi",options)
-
     tukey.startGame()
     print ("最后盈利为：" + str(tukey.money))
     print ("最后一期盈利大小：" + str(tukey.lastIswinMoney))
     print ("总中奖次数：" + str(tukey.winCount))
+    print ("总游戏次数：" + str(tukey.playCount))
+    '''
+    
+    #以赌徒模式来计算，上下止盈、封顶均为10000 ,那么总次数为
+    winCount = 0
+    winMoney = 0
+    gameCount = 0
+    for i in range(0,10) :
+        (options, args) = parser.parse_args()
+        tukey=Turkey(i,options)
+        thisWin=tukey.startGame()
+        if thisWin > 0 :
+            print "winCount:"+str(winCount)
+            winCount = winCount + 1
+        winMoney = winMoney + tukey.money
+        gameCount = gameCount + tukey.playCount
+        print ("第 %d 次总游戏次数："  %(i)  + str(tukey.playCount))
+        print ("第 %d 次盈利为："  %(i)  + str(tukey.money))
+        
+    print ("大轮游戏赢次：" + str(winCount))
+    print ("小轮游戏总次数：" + str(gameCount))
+    print ("最后盈利为：" + str(winMoney))
+  
+    
 
